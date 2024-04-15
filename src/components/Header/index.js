e53db1ee808a884ccf723829e4543bcaeed5eac3
cmouli692 <138131601+cmouli692@users.noreply.github.com>
2024-04-15 14:14:17 +0530

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -10,19 +10,19 @@ const Header = () =>{
    const brandNameAndControlIcons = () =>(
         <div className ="header-and-search-logos-container">
            <div>
-            <img src="../.././tanTrimLogo.png" alt="tanTrimLogo"/>
-             {/* <h1 className="brand-name">TANN TRIM</h1> */}
+      <img src="../.././tanTrimLogo.png" alt="tanTrimLogo"/>
+            {/* <h1 className="brand-name">TANN TRIM</h1> */}
            </div> 
            <div className="header-icons-container">
-             {/* <IoIosSearch color="#e3e8e8"/> */}
-             <img src="../.././searchIcon.png" alt="searchIcon"/>
-             <img src="../.././userIcon.png" alt="searchIcon"/>
-             {/* <BsFillPersonFill color="#e3e8e8"/> */}
-             <img src="../.././bookMarkIcon.png" alt="searchIcon"/>
-             {/* <BsBookmark color="#e3e8e8"/> */}
+              <IoIosSearch color="#e3e8e8"/>
+             {/*<img src="../.././searchIcon.png" alt="searchIcon"/> */}
+           {/*  <img src="../.././userIcon.png" alt="searchIcon"/> */}
+              <BsFillPersonFill color="#e3e8e8"/>
+            {/* <img src="../.././bookMarkIcon.png" alt="searchIcon"/> */}
+              <BsBookmark color="#e3e8e8"/>
 
-             <img src="../.././shoppingBagIcon.png" alt="shoppingBagIcon"/>
-             {/* <MdShoppingBag   color="#e3e8e8"/> */}
+             {/*  <img src="../.././shoppingBagIcon.png" alt="shoppingBagIcon"/> */}
+            <MdShoppingBag   color="#e3e8e8"/>
            </div>
         </div>
 
